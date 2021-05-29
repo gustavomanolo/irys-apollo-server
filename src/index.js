99693bd06a90ac2c5f4206bda35f6ffd65223528
Gustavo Manolo <gustavomanolo@gmail.com>
2021-05-28 19:55:53 -0500

    quick fix

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 const { ApolloServer } = require("apollo-server");
 const typeDefs = require("./apollo/schema");
 const resolvers = require("./apollo/resolvers");
-const connectDB = require('../config/db')
-connectDB()
+const connectDB = require("../config/db");
+connectDB();
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
-server.listen().then(({ url }) => {
-  console.log(`GraphQL server running at: ${url}`);
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+  console.log(`GraphQL server running at ${url}`);
 });
